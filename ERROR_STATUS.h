3c0fc4cde2da455519078e78b482a1dc590b6a76
OmarElganzory <62390601+omarganzory@users.noreply.github.com>
2020-03-28 09:28:30 +0200

    Add files via upload

---
+++ ERROR_STATUS.h
@@ -0,0 +1,31 @@
+/*
+ *
+ *  ERROR.h
+ *
+ *******Created on  : Mar 26, 2020
+ *******Author      : OMAR ELGANZORY
+ *******Description : Error Statues lib
+ *
+ */
+ 
+ 
+/*************************************************************
+***         Guard Of File Will Call One Time IN .c         ***
+**************************************************************/
+
+
+#ifndef LIB_ERROR_STATE_H_
+#define LIB_ERROR_STATE_H_
+
+typedef enum
+
+{
+	ES_OK,
+	ES_NOK,
+	ES_OUT_OF_RANGE,
+	ES_NULL_POINTER
+
+}ErrorStatus;
+
+
+#endif /* LIB_ERROR_STATE_H_ */
---
+++ STD_TYPES.h.h
@@ -0,0 +1,32 @@
+/*
+ *
+ * STD_TYPES.h
+ *
+ *******Created on  : Mar 26, 2020
+ *******Author      : OMAR ELGANZORY
+ *******Version     : 1.0 V 
+ *
+ */
+ 
+
+/*************************************************************
+***         Guard Of File Will Call One Time IN .c         ***
+**************************************************************/
+
+
+#ifndef LIB_STD_TYPES_H_
+#define LIB_STD_TYPES_H_
+
+typedef   unsigned   char       u8;
+typedef   unsigned   short int  u16;
+typedef   unsigned   long  int  u32;
+
+typedef   signed     char       s8;
+typedef   signed     short int  s16;
+typedef   signed     long  int  s32;
+
+typedef   float                 f32;
+typedef   double                f64;
+typedef   long       double     f96;
+
+#endif /* LIB_STD_TYPES_H_ */
